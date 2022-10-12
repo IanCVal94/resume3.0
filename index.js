2dc720d4a20b25f8a02c75ed6911cbf503e57ab2
IanCVal <86568417+IanCVal94@users.noreply.github.com>
2022-10-12 18:28:12 -0500

    added photos and added lockheed martin intern section

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ const PORT = process.env.PORT || 8080;
 
 console.log('Server listening on:', PORT);
 app.listen(PORT);
+
+
+//Its a node program that creates an HTTP server to send the resume file on all requests
+//Git contains my resume file I have on my PC so I may directly update the file from VS code and the update flows through all versions of it on Git and anything that is connected to the Git file
+//Heroku takes the resume file I have created and added to my github library so I may create a domain link that says something like icvalderas.com
+
+
+//to update, put these commands into the terminal:
+//git add .
+//git commit -m "message to relay the notes of the update"
+//git push
